4dd7fce2109e3869ddf20c61666d39e2164e7bca
Owner <shayleebaumgartner2002@gmail.com>
2024-04-02 13:08:04 -0400

    shortening code

--- js/models.js
+++ js/models.js
@@ -2,7 +2,7 @@
 const BASE_URL = "https://hack-or-snooze-v3.herokuapp.com";
 
 let currUser = localStorage.username;
-let userToken = localStorage.token; 
+let userToken = localStorage.token;
 
 class Story {
   constructor({ storyId, title, author, url, username, createdAt }) {
@@ -13,9 +13,9 @@ class Story {
     this.username = username;
     this.createdAt = createdAt;
   }
-  //This is the parens that appear after the title of the stories the User uploads. 
+
   getHostName() {
-    return "hostname.com";
+    return new URL(this.url).hostname;
   }
 }
 
@@ -39,63 +39,29 @@ class StoryList {
     const response = await axios({
       url: `${BASE_URL}/stories`,
       method: "POST",
-      data: { token: userToken, story: {author: `${author}`, title: `${title}`, url: `${url}`} },
+      data: { token: userToken, story: { author, title, url} },
     });
-   
-    const story = new StoryList();
-    const storyId = await story.getStoryId();
-    const createdAt = await story.getDateAndTime();
-    const newStory = new Story({ storyId, title, author, url, createdAt, username });
-    createUserStoryUI(newStory);
-    response; 
 
-    //below tests if the submit button to add a story was pressed while ON the "My Stores" tab and prepends the story right then (instead of waiting for the "My Stories" button to be clicked to prepend the story)
+    const newStory = new Story(response.data.story);
+    createUserStoryUI(newStory);
+  
     const userStories = $('#my-stories-list');
-    if(!(userStories.hasClass('hide'))){
+    if (!(userStories.hasClass('hide'))) {
       addingMyStoriesToTab();
     }
-
-  }
-
-  async getStoryId(){
-    const response = await axios({
-      url: `${BASE_URL}/users/${currUser}`,
-      method: "GET",
-      params: {token: userToken}
-    });
-
-    const myUser = response.data.user.stories;
-    const lastItem = myUser[myUser.length -1];
-    const myId = lastItem.storyId; 
-
-    return myId; 
-  }
-
-  async getDateAndTime(){
-    const response = await axios({
-      url: `${BASE_URL}/users/${currUser}`,
-      method: "GET",
-      params: {token: userToken}
-    });
-
-    const myUser = response.data.user.stories;
-    const lastItem = myUser[myUser.length -1];
-    const dateAndTime = lastItem.createdAt; 
-
-    return dateAndTime; 
   }
 
 }
 
 class User {
   constructor({
-                username,
-                name,
-                createdAt,
-                favorites = [],
-                ownStories = []
-              },
-              token) {
+    username,
+    name,
+    createdAt,
+    favorites = [],
+    ownStories = []
+  },
+    token) {
     this.username = username;
     this.name = name;
     this.createdAt = createdAt;
@@ -173,43 +139,43 @@ class User {
       return null;
     }
   }
-  
-  static async removeStoryFromFavorites(li){
+
+  static async removeStoryFromFavorites(li) {
     const starId = li.getAttribute('id');
     const favs = $('#favorites-list');
     const items = favs.children();
-  
-      for(let i = 0; i < items.length; i++){
-        const index = items[i];
-        const indexId = index.getAttribute('id');
-  
-        if(indexId === starId){
-          index.remove();
-          const removeFavorite = axios.delete(`${BASE_URL}/users/${currUser}/favorites/${indexId}?token=${userToken}`);
-          removeFavorite; 
-        }
+
+    for (let i = 0; i < items.length; i++) {
+      const index = items[i];
+      const indexId = index.getAttribute('id');
+
+      if (indexId === starId) {
+        index.remove();
+        const removeFavorite = axios.delete(`${BASE_URL}/users/${currUser}/favorites/${indexId}?token=${userToken}`);
+        removeFavorite;
       }
+    }
   }
-  
-  static async removeStarFromStoryList(li){
+
+  static async removeStarFromStoryList(li) {
     const storiesContainer = $('#all-stories-list');
     const itemsj = storiesContainer.children();
     const starId = li.getAttribute('id');
-  
-    for(let j = 0; j < itemsj.length; j++){
+
+    for (let j = 0; j < itemsj.length; j++) {
       const indexj = itemsj[j];
       const indexIdj = indexj.getAttribute('id');
-  
-        if(starId === indexIdj){
-          const span = indexj.children[0];
-          const svg = span.children[0];
-          const path = svg.children[0];
-          path.setAttribute('d', 'M287.9 0c9.2 0 17.6 5.2 21.6 13.5l68.6 141.3 153.2 22.6c9 1.3 16.5 7.6 19.3 16.3s.5 18.1-5.9 24.5L433.6 328.4l26.2 155.6c1.5 9-2.2 18.1-9.7 23.5s-17.3 6-25.3 1.7l-137-73.2L151 509.1c-8.1 4.3-17.9 3.7-25.3-1.7s-11.2-14.5-9.7-23.5l26.2-155.6L31.1 218.2c-6.5-6.4-8.7-15.9-5.9-24.5s10.3-14.9 19.3-16.3l153.2-22.6L266.3 13.5C270.4 5.2 278.7 0 287.9 0zm0 79L235.4 187.2c-3.5 7.1-10.2 12.1-18.1 13.3L99 217.9 184.9 303c5.5 5.5 8.1 13.3 6.8 21L171.4 443.7l105.2-56.2c7.1-3.8 15.6-3.8 22.6 0l105.2 56.2L384.2 324.1c-1.3-7.7 1.2-15.5 6.8-21l85.9-85.1L358.6 200.5c-7.8-1.2-14.6-6.1-18.1-13.3L287.9 79z');
-        }
-    } 
+
+      if (starId === indexIdj) {
+        const span = indexj.children[0];
+        const svg = span.children[0];
+        const path = svg.children[0];
+        path.setAttribute('d', 'M287.9 0c9.2 0 17.6 5.2 21.6 13.5l68.6 141.3 153.2 22.6c9 1.3 16.5 7.6 19.3 16.3s.5 18.1-5.9 24.5L433.6 328.4l26.2 155.6c1.5 9-2.2 18.1-9.7 23.5s-17.3 6-25.3 1.7l-137-73.2L151 509.1c-8.1 4.3-17.9 3.7-25.3-1.7s-11.2-14.5-9.7-23.5l26.2-155.6L31.1 218.2c-6.5-6.4-8.7-15.9-5.9-24.5s10.3-14.9 19.3-16.3l153.2-22.6L266.3 13.5C270.4 5.2 278.7 0 287.9 0zm0 79L235.4 187.2c-3.5 7.1-10.2 12.1-18.1 13.3L99 217.9 184.9 303c5.5 5.5 8.1 13.3 6.8 21L171.4 443.7l105.2-56.2c7.1-3.8 15.6-3.8 22.6 0l105.2 56.2L384.2 324.1c-1.3-7.7 1.2-15.5 6.8-21l85.9-85.1L358.6 200.5c-7.8-1.2-14.6-6.1-18.1-13.3L287.9 79z');
+      }
+    }
   }
 
-  static async addStoryToFavorites(clone){
+  static async addStoryToFavorites(clone) {
     const storyId = clone.getAttribute('id');
 
     const cloneStar = clone.childNodes.item(1);
@@ -217,16 +183,16 @@ class User {
     const pathStar = makingStar.children[0];
     pathStar.setAttribute('d', 'M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z');
     makingStar.setAttribute('data-prefix', 'fas');
-   
+
     const updateFavs = await axios.post(`${BASE_URL}/users/${currUser}/favorites/${storyId}?token=${userToken}`);
-    updateFavs; 
+    updateFavs;
   }
 
-  static async removeUsersStory(event){
+  static async removeUsersStory(event) {
     const trash = event.target.closest('.trash-can');
     const usersStories = $('#my-stories-list');
     const storiesChildren = usersStories.children();
-  
+
     if (trash !== null) {
       const li = trash.closest('li');
       const id = li.getAttribute('id');
@@ -234,12 +200,12 @@ class User {
       res;
 
       li.remove();
-     
-        if(storiesChildren.length === 2){
-          const noStoriesMsg = $('#no-stories-msg');
-          noStoriesMsg.removeClass('hide');
-        }
-    
+
+      if (storiesChildren.length === 2) {
+        const noStoriesMsg = $('#no-stories-msg');
+        noStoriesMsg.removeClass('hide');
+      }
+
     }
   }
 }
--- js/stories.js
+++ js/stories.js
@@ -32,7 +32,7 @@ function generateStoryMarkup(story) {
 function putStoriesOnPage() {
   console.debug("putStoriesOnPage");
 
-  if($allStoriesList.attr('name')){
+  if ($allStoriesList.attr('name')) {
     $allStoriesList.show();
     console.log('has attr');
   } else {
@@ -49,7 +49,7 @@ function putStoriesOnPage() {
 }
 
 //called on the nav.js to grab the values of the form after the User hits "Submit" to upload a story. We create a new instance of a story, then pass the values into the addStory method in the StoryList class. 
-async function getStorySubmitData(evt){
+async function getStorySubmitData(evt) {
   evt.preventDefault();
   const story = new StoryList();
   const author = $('#story-author').val();
@@ -59,49 +59,49 @@ async function getStorySubmitData(evt){
   story.addStory(title, author, url, currUser);
 }
 
-  //where should this function be? 
-  function currentFavoritesOnPage(){
-    const favsListUI = $('#favorites-list');
-    const favsItemsUI = favsListUI.children();
-    const array = [];
-    for(let i = 0; i < favsItemsUI.length; i++){
-      const li = favsItemsUI[i];
-      const id = li.getAttribute('id');
-      array.push(id);
+//where should this function be? 
+function currentFavoritesOnPage() {
+  const favsListUI = $('#favorites-list');
+  const favsItemsUI = favsListUI.children();
+  const array = [];
+  for (let i = 0; i < favsItemsUI.length; i++) {
+    const li = favsItemsUI[i];
+    const id = li.getAttribute('id');
+    array.push(id);
+  }
+
+  return array;
+}
+
+//this runs a check to see if any of the stars on the home page or in the favorites list, then it will color it's star
+async function loadColoredStarsOnHome() {
+  const ol = $('#all-stories-list');
+  const olItems = ol.children();
+
+  for (let i = 0; i < olItems.length; i++) {
+    const story = olItems[i];
+    const id = story.getAttribute('id');
+    const array = getIdOfUserFavorites();
+    const arrayR = Promise.resolve(array);
+    const arrayA = await arrayR;
+
+    if (arrayA.includes(id)) {
+      const storySpan = story.children[0];
+      const storySvg = storySpan.children[0];
+      const storyPath = storySvg.children[0];
+      storyPath.setAttribute('d', 'M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z');
     }
 
-    return array; 
   }
-
-  //this runs a check to see if any of the stars on the home page or in the favorites list, then it will color it's star
-  async function loadColoredStarsOnHome(){
-    const ol = $('#all-stories-list');
-    const olItems = ol.children();
-
-    for(let i = 0; i < olItems.length; i++){
-      const story = olItems[i];
-      const id = story.getAttribute('id');
-      const array = getIdOfUserFavorites();
-      const arrayR = Promise.resolve(array);
-      const arrayA = await arrayR;
-     
-      if(arrayA.includes(id)){
-        const storySpan = story.children[0];
-        const storySvg = storySpan.children[0];
-        const storyPath = storySvg.children[0];
-        storyPath.setAttribute('d', 'M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z');
-      }
-
-    } 
-  }
+}
 
 //This tests the length of how many stories are inside the User's created Stories and shows or hides the message about no stories. 
-function testingIfStoriesEmpty(){
+function testingIfStoriesEmpty() {
   const myStoriesList = $('#my-stories-list');
   const children = myStoriesList.children();
   const noStoriesMsg = $('#no-stories-msg');
 
-  if(children.length === 1){
+  if (children.length === 1) {
     noStoriesMsg.removeClass('hide');
   } else {
     noStoriesMsg.addClass('hide');
@@ -109,20 +109,20 @@ function testingIfStoriesEmpty(){
 }
 
 //this loops through the stories currently in the user's personal story list to help pervent from loading the name stories over and over
-function getIdOfUserStories(){
+function getIdOfUserStories() {
   const myStoriesList = $('#my-stories-list');
   const items = myStoriesList.children();
   const array = [];
-  
-  for(let story of items){
+
+  for (let story of items) {
     array.push(story.getAttribute('id'));
   }
 
-  return array; 
+  return array;
 }
 
 //this function creates the UI of the user's own stories being appended onto the page.
-function createUserStoryUI(story){
+function createUserStoryUI(story) {
   const li = $(`
   <li id="${story.storyId}" name="myStory" class="story-li">
   <span class="star">
@@ -138,7 +138,7 @@ function createUserStoryUI(story){
   <hr>
 `);
 
-$('#all-stories-list').prepend(li);
+  $('#all-stories-list').prepend(li);
 }
 
 //this document.addEventListener checks if the user presses the trash button on the "My Stories tab"
@@ -146,50 +146,50 @@ document.addEventListener('click', User.removeUsersStory);
 
 //MANAGING THE FAVORITES 
 //below determines if the star you clicked is colored (meaning you already clicked it once) or not, to determine the correct action in updating the favorites tab. 
-document.addEventListener('click', function(event){
+document.addEventListener('click', function (event) {
   const star = event.target.closest('.fa-star');
   if (star !== null) {
     //Removes the color of the star because it was clicked and colored
-    if(star.hasAttribute('name')){
+    if (star.hasAttribute('name')) {
       star.classList.remove('fas');
       star.classList.add('far');
       star.removeAttribute('name');
-  
+
       updateFavoriteStories(star);
     } else {
       //Adds color to the star because it was unclicked and uncolored
       star.classList.remove('far');
       star.classList.add('fas');
       star.setAttribute('name', 'colored');
-    
+
       updateFavoriteStories(star);
     }
-}
+  }
 })
 
 //First tests if the star is colored by if the class contains 'fas' (meaning the star is now colored since the user just clicked it). If the star is now colored, we add a clone of it to the favorites tab. 
-function updateFavoriteStories(star){
+function updateFavoriteStories(star) {
   const li = star.closest("li");
   const parentOl = star.closest('ol');
   const clone = li.cloneNode(true);
   const parentId = parentOl.getAttribute('id');
   const favsOl = $('#favorites-list');
 
-  if(star.classList.contains('fas')){
+  if (star.classList.contains('fas')) {
     User.addStoryToFavorites(clone);
   } else {
- //If the star does NOT contain 'fas', we run another if statement to see the action of removing the star color was made in the Favorites tab OR the Hacker News tab. 
- //If it was in the Favorites Tab we we have to remove the star from the Hacker Snooze tab AND the Favorites tab while removing the whole story from the Favorites tab. (If the star was clicked on the Hacker Snooze tab the star was already removed, it was clicked on the Favorites tab that star was removed and we need to remove it from the Hacker Snooze tab also).
-    if(parentId === 'favorites-list'){
+    //If the star does NOT contain 'fas', we run another if statement to see the action of removing the star color was made in the Favorites tab OR the Hacker News tab. 
+    //If it was in the Favorites Tab we we have to remove the star from the Hacker Snooze tab AND the Favorites tab while removing the whole story from the Favorites tab. (If the star was clicked on the Hacker Snooze tab the star was already removed, it was clicked on the Favorites tab that star was removed and we need to remove it from the Hacker Snooze tab also).
+    if (parentId === 'favorites-list') {
       User.removeStarFromStoryList(li);
       User.removeStoryFromFavorites(li);
-      if(favsOl.length === 1){
+      if (favsOl.length === 1) {
         const noFavsMsg = $('#no-favs-msg');
         noFavsMsg.removeClass('hide');
       }
     } else {
       User.removeStoryFromFavorites(li);
-      if(favsOl.length === 1){
+      if (favsOl.length === 1) {
         const noFavsMsg = $('#no-favs-msg');
         noFavsMsg.removeClass('hide');
       }
@@ -199,16 +199,17 @@ function updateFavoriteStories(star){
 }
 
 //this finds the ids of the stories that are currently in the User's saved favorites object list
-async function getIdOfUserFavorites(){
-  const res = await axios.get(`${BASE_URL}/users/${currUser}?token=${userToken}`);
-  const resR = Promise.resolve(res);
-  const resA = await resR; 
-  const userFavs = resA.data.user.favorites; 
+async function getIdOfUserFavorites() {
   const array = [];
-  
-  for(let fav of userFavs){
-    array.push(fav.storyId);
+  if (currUser) {
+    const res = await axios.get(`${BASE_URL}/users/${currUser}?token=${userToken}`);
+    const resR = Promise.resolve(res);
+    const resA = await resR;
+    const userFavs = resA.data.user.favorites;
+    for (let fav of userFavs) {
+      array.push(fav.storyId);
+    }
   }
 
-  return array; 
+  return array;
 }
